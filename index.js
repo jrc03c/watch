aacd2f90d33c226ca99e0fe5575beb0f74097984
jrc03c <jrc03c@pm.me>
2021-09-06 09:37:21 -0500

    ...

--- index.js
+++ index.js
@@ -5,35 +5,37 @@ const pause = require("./pause.js")
 const getFilesRecursive = require("./get-files-recursive.js")
 const getHash = require("./get-hash.js")
 
-const target = path.resolve(
-  process.argv.slice(2).filter(a => !a.startsWith("--"))[0]
-)
+function watch(target, callback) {
+  target = path.resolve(target)
 
-const files = fs.lstatSync(target).isFile()
-  ? [target]
-  : getFilesRecursive(target)
+  const files = fs.lstatSync(target).isFile()
+    ? [target]
+    : getFilesRecursive(target)
 
-const dict = {}
-let index = 0
-let isBusy = false
+  const dict = {}
+  let index = 0
+  let isBusy = false
 
-setInterval(async () => {
-  if (isBusy) return
-  isBusy = true
+  setInterval(async () => {
+    if (isBusy) return
+    isBusy = true
 
-  const file = files[index]
-  const newHash = await getHash(file)
+    const file = files[index]
+    const newHash = await getHash(file)
 
-  if (!dict[file]) {
-    dict[file] = newHash
-  } else {
-    if (newHash !== dict[file]) {
-      console.log(`${file} has changed!`)
+    if (!dict[file]) {
+      dict[file] = newHash
+    } else {
+      if (newHash !== dict[file]) {
+        callback(file)
+      }
+
+      dict[file] = newHash
     }
 
-    dict[file] = newHash
-  }
+    index = (index + 1) % files.length
+    isBusy = false
+  }, 0)
+}
 
-  index = (index + 1) % files.length
-  isBusy = false
-}, 0)
+module.exports = watch
